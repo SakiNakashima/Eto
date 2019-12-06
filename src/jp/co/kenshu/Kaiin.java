20a4c886bbf5e14f1280d6004c596b715a0b6c61
SakiNakashima <s.nakashima94@gmail.com>
2019-12-06 16:40:52 +0900

    Q4finish

---
+++ src/jp/co/kenshu/Kaiin.java
@@ -0,0 +1,36 @@
+package jp.co.kenshu;
+
+import java.util.Date;
+
+public class Kaiin {
+	private int id;
+	private String name;
+	private Date addDate;
+	Kaiin(){};
+	
+	Date date=new Date();
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id =id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public Date getDate() {
+		return addDate;
+	}
+	
+	public void setDate(Date addDate) {
+		this.addDate =addDate;
+	}
+}
